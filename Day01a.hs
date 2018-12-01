d1568bc08e5b49fa918bcd1363f76fd0655a66d8
Mark Gerarts <mark.gerarts@gmail.com>
2018-12-01 10:00:09 +0100

    Initial commit

---
+++ Day01a.hs
@@ -0,0 +1,10 @@
+module Day01a where
+
+main :: IO ()
+main = do
+  s <- readFile "input/Day01a.txt"
+  print . sum . map string2int . lines $ s
+
+string2int :: String -> Int
+string2int x | head x == '+' = read $ tail x
+             | otherwise = read x
---
+++ Day01b.hs
@@ -0,0 +1,20 @@
+module Day01b where
+
+main :: IO ()
+main = do
+  s <- readFile "input/Day01a.txt"
+  let integerList = map string2int . lines $ s
+    in print $ findDuplicate (cycle integerList) [0]
+
+string2int :: String -> Int
+string2int x | head x == '+' = read $ tail x
+             | otherwise = read x
+
+findDuplicate :: [Int] -> [Int] -> Int
+findDuplicate [] [x] = x
+findDuplicate [] _ = error "No duplicate found"
+findDuplicate deltas frequencies
+  | newFrequency `elem` frequencies = newFrequency
+  | otherwise = findDuplicate (tail deltas) (frequencies ++ [newFrequency])
+  where
+    newFrequency = head deltas + last frequencies
