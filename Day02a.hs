5b8adc42e0a4188128a0735f186dd6a5dc405f0f
Mark Gerarts <mark.gerarts@gmail.com>
2018-12-02 10:14:59 +0100

    Solve day 2

---
+++ Day02a.hs
@@ -0,0 +1,38 @@
+module Day02a where
+
+import Data.List
+import Data.Maybe
+
+main :: IO ()
+main = do
+  s <- readFile "input/Day02a.txt"
+  print $ solution s
+
+solution :: String -> String
+solution s = show $ numberOf2s * numberOf3s
+  where
+    occurenceList = (map get2And3Occurrences . lines) s
+    numberOf2s = foldl (\count (twos, _) -> if twos then count + 1 else count) 0 occurenceList
+    numberOf3s = foldl (\count (_, threes) -> if threes then count + 1 else count) 0 occurenceList
+
+sampleInput :: [String]
+sampleInput =
+  [
+    "abcdef",
+    "bababc",
+    "abbcde",
+    "abcccd",
+    "aabcdd",
+    "abcdee",
+    "ababab"
+  ]
+
+countOccurrences :: Char -> String -> Int
+countOccurrences c = length . filter (==c)
+
+get2And3Occurrences :: String -> (Bool, Bool)
+get2And3Occurrences s = (isJust has2s, isJust has3s)
+  where
+    counts = map (`countOccurrences` s) (nub s)
+    has2s = find (==2) counts
+    has3s = find (==3) counts
---
+++ Day02b.hs
@@ -0,0 +1,29 @@
+module Day02b where
+
+import Data.List
+import Data.Maybe
+
+main :: IO ()
+main = do
+  s <- readFile "input/Day02a.txt"
+  print $ solve $ lines s
+
+solve :: [String] -> String
+solve xs = a `intersect` b
+  where
+    (a, b) = getMatchingIds xs
+
+differsByOne :: Eq a => [a] -> [a] -> Int -> Bool
+differsByOne s1 s2 numberOfDiffers
+  | numberOfDiffers > 1 = False
+  | s1 == s2 && null s1 = True
+  | otherwise = differsByOne (tail s1) (tail s2) newInc
+  where
+    shouldInc = head s1 /= head s2
+    newInc = if shouldInc then numberOfDiffers + 1 else numberOfDiffers
+
+getMatchingIds :: [String] -> (String, String)
+getMatchingIds = fromJust . find (\(x,y) -> differsByOne x y 0) . toPairs
+
+toPairs :: [String] -> [(String, String)]
+toPairs xs = xs >>= (\x -> zip (repeat x) (filter (/=x) xs))
