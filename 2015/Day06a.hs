70809bad365e5613d1e1dfdacd494fb7a74cd358
Mark Gerarts <mark.gerarts@gmail.com>
2019-01-14 16:28:56 +0100

    Solve 2015-06-a

--- 2015/Day06a.hs
+++ 2015/Day06a.hs
@@ -1,11 +1,14 @@
 module Day06a where
 
 import Text.Regex.PCRE
-import qualified Data.Map.Strict as M
+import qualified Data.Set as S
+import Data.List
 
+data Action = TurnOn | TurnOff | Toggle
 type Coordinate = (Int, Int)
 type Rectangle = (Coordinate, Coordinate)
-type Grid = M.Map Coordinate Bool
+type Grid = S.Set Coordinate
+type Instruction = (Action, Rectangle)
 
 main :: IO ()
 main = do
@@ -13,24 +16,33 @@ main = do
   print $ solve s
 
 solve :: String -> Int
-solve s = M.size $ M.filter (==True) $ foldl (\g i -> i g) M.empty instructions
+solve s = S.size $ foldl' (flip performInstruction) S.empty instructions
   where
-    instructions = map (toggleRectangle . parse) $ lines s
+    instructions = map parse $ lines s
 
-parse :: String -> Rectangle
-parse s = ((x1, y1), (x2, y2))
+parse :: String -> Instruction
+parse s = (action, rect)
   where
     numbers = getAllTextMatches (s =~ "\\d+" :: AllTextMatches [] String)
     [x1, y1, x2, y2] = map read numbers
+    rect = ((x1, y1), (x2, y2))
+    action | "turn off" `isPrefixOf` s = TurnOff
+           | "turn on" `isPrefixOf` s = TurnOn
+           | "toggle" `isPrefixOf` s = Toggle
+           | otherwise = error "No parse"
+
 
 rectangleToCoordinates :: Rectangle -> [Coordinate]
 rectangleToCoordinates ((x1, y1), (x2, y2)) =
   [(x, y) | x <- [x1..x2], y <- [y1..y2]]
 
-toggleCoordinate :: Coordinate -> Grid -> Grid
-toggleCoordinate c = M.insertWith (\_ old -> not old) c True
+performInstruction :: Instruction -> Grid -> Grid
+performInstruction (action, rect) g =
+  foldl (\g' c -> doForCoordinate action c g') g (rectangleToCoordinates rect)
 
-toggleRectangle :: Rectangle -> Grid -> Grid
-toggleRectangle r g = foldl (flip toggleCoordinate) g coords
-  where
-    coords = rectangleToCoordinates r
+
+doForCoordinate :: Action -> Coordinate -> Grid -> Grid
+doForCoordinate TurnOn c g = S.insert c g
+doForCoordinate TurnOff c g = S.delete c g
+doForCoordinate Toggle c g | S.member c g = S.delete c g
+                           | otherwise = S.insert c g
