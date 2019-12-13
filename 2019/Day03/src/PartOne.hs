4ce3bb2bc3080a034d972d5c4131f469184f831b
Mark Gerarts <mark.gerarts@gmail.com>
2019-12-13 22:28:03 +0100

    Solve 03-a in a semi-performant way

--- 2019/Day03/src/PartOne.hs
+++ 2019/Day03/src/PartOne.hs
@@ -8,17 +8,16 @@ import qualified Data.Set as S
 type Position = (Int, Int)
 data Direction = Up | Right | Down | Left deriving (Show)
 type Instruction = (Direction, Int)
-type Path = [Position]
-type Line = (Position, Position)
-type Wire = [Line]
+type Wire = [Position]
 
 main :: IO ()
 main = do
   input <- readFile "input.txt"
-  print $ distanceToNearest (0, 0) $ getIntersections $ parseInput input
-
--- @todo: rework with segments and their intersections. It isn't feasible
---        to story every single position.
+  let wires = parseInput input
+      w1 = head wires
+      w2 = last wires
+      intersections = getIntersections w1 w2
+  print $ distanceToNearest (0, 0) intersections
 
 parseDirection :: Char -> Direction
 parseDirection input = case input of
@@ -28,51 +27,36 @@ parseDirection input = case input of
   'L' -> Left
   _ -> error "Invalid direction"
 
+-- "Walks" according to the instructions, collecting the passed points and thus
+-- forming the wire.
+walk :: Position -> [Instruction] -> [Position]
+walk s is = nub $ foldl (\c i -> c ++ lineTo (last c) i) [(0, 0)] is
+  where
+    lineTo (sx, sy) i = case i of
+      (Up, n) -> [(sx, y) | y <- [sy..(sy + n)]]
+      (Down, n) -> [(sx, y) | y <- [sy,(sy - 1)..(sy - n)]]
+      (Right, n) -> [(x, sy) | x <- [sx..(sx + n)]]
+      (Left, n) -> [(x, sy) | x <- [sx,(sx - 1)..(sx - n)]]
+
+parseInput :: String -> [Wire]
+parseInput = map parseWire . lines
+
+parseWire :: String -> Wire
+parseWire = walk (0,0) . map parseInstruction . splitOn ","
+
 parseInstruction :: String -> Instruction
 parseInstruction input = (direction, amount)
   where
     direction = parseDirection $ head input
     amount = read $ tail input
 
--- Parses the input in a list of wires
-parseInput :: String -> [Wire]
-parseInput = map (pathToWire . walk (0, 0) . parseInstructions) . lines
-
-parseInstructions :: String -> [Instruction]
-parseInstructions = map parseInstruction . splitOn ","
-
--- Constructs a path of points which form the lines of the wire
-walk :: Position -> [Instruction] -> Path
-walk start = foldl (\path instruction -> path ++ [nextPosition path instruction]) [start]
+getIntersections :: Wire -> Wire -> [Position]
+getIntersections w1 w2 = S.toList $ S.delete (0, 0) intersections
   where
-    nextPosition path = followInstruction (last path)
-
-pathToWire :: Path -> Wire
-pathToWire path = zip path (tail path)
-
-followInstruction :: Position -> Instruction -> Position
-followInstruction (startX, startY) (direction, amount) =
-    case direction of
-        Up -> (startX, startY + amount)
-        Down -> (startX, startY - amount)
-        Right -> (startX + amount, startY)
-        Left -> (startX - amount, startX)
-
-getIntersections :: [Wire] -> [Position]
-getIntersections paths = undefined
-
-intersects :: Line -> Line -> Bool
-intersects ((ax1, ay1), (ax2, ay2)) ((bx1, by1), (bx2, by2)) = regularIntersect
-    where
-      regularIntersect = ((ax1 <= bx1 && ax2 >= bx1) || (ax2 <= bx1 && ax1 >= bx1))
-        && ((by1 <= ay1 && by2 >= ay1) || (by2 <= ay1 && by1 >= ay1))
--- @todo
+    intersections = S.intersection (S.fromList w1) (S.fromList w2)
 
 distanceToNearest :: Position -> [Position] -> Int
 distanceToNearest start positions = minimum $ map (distance start) positions
 
 distance :: Position -> Position -> Int
-distance (x1, y1) (x2, y2) = abs (x1 - x2) +  abs(y1 - y2)
-
-testInput :: String
-testInput = "R8,U5,L5,D3\nU7,R6,D4,L4"
+distance (x1, y1) (x2, y2) = abs (x1 - x2) +  abs (y1 - y2)
